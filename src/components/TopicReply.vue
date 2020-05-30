29907b87512f8119918e6cfc16555b4589bd2dd9
ChenLiang <236338364@qq.com>
2020-05-31 01:30:39 +0800

    topicDetail

---
+++ src/components/TopicReply.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="topic-reply">
+    <div class="reply-item" v-for="(item, index) in arr" :key="index">
+      <img class="avatar" :src="obj.src" />
+      <div class="right-item">
+        <div class="reply-title">
+          <span class="username"> 1 XanderChen</span>
+          <span class="replytime"> 4 分钟前</span>
+        </div>
+        <div class="reply-content">
+          <p>曾经也是这样, 不过不是因为想暴富而是纯属想抢注。</p>
+          <p>
+            直到我开始把域名纳入 "订阅" 记账范围, 最终发现自己的域名, 其实就是
+            "毫无价值的撒钱"。它不会给我带来任何形式的利益,
+            支付的几百块其实完全就是撒出去了。就算我拿这钱到某个群里发个红包,
+            它的回馈都会比我买域名的回馈多得多。
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TopicReply',
+  data() {
+    return {
+      obj: {
+        src: require('@/assets/avatar.jpg')
+      },
+      arr: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/scss/variable.scss';
+
+.topic-reply {
+  background: $color-white;
+  border-radius: 4px;
+  margin-bottom: 20px;
+  padding: 10px 0;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1);
+  border-bottom: 1px solid #e2e2e2;
+}
+.reply-item {
+  display: flex;
+  padding: 10px;
+  border-bottom: 1px solid #e2e2e2;
+  &:last-child {
+    border-bottom: none;
+  }
+}
+.avatar {
+  width: 50px;
+  height: 50px;
+  border-radius: 4px;
+  margin-right: 10px;
+  flex: 0 0 auto;
+}
+.right-item {
+  display: flex;
+  flex-flow: column;
+  text-align: left;
+}
+.reply-title {
+  .username {
+    color: $color-gray;
+    font-weight: bold;
+  }
+  .replytime {
+    color: #cac7c7;
+    margin-left: 20px;
+    font-size: 0.8rem;
+  }
+}
+.reply-content {
+  line-height: 1.5rem;
+  margin-top: 20px;
+  padding-right: 20px;
+  font-size: 0.9rem;
+}
+</style>
--- src/views/TopicDetail.vue
+++ src/views/TopicDetail.vue
@@ -1,18 +1,99 @@
 <template>
-  <div>
+  <div class="topic-detail">
     <Header ref="header" formStatus />
+    <div class="container page-width">
+      <div class="left">
+        <div class="topic-top">
+          <div class="topic-header">
+            <div class="header-left">
+              <h3 class="title">
+                社区/论坛类的是不是不太行了，怎么还没看见前后端分离的成熟产品呢？
+              </h3>
+            </div>
+            <img class="avatar" :src="topic.src" alt="" />
+          </div>
+          <div class="topic-content">
+            <p>每天写代码不能进步</p>
+            <p>如果作为产品或者其他的 能想到把积累的数据产生回报</p>
+            <p>程序员如何往这方面发展呢 比如我有一个 idea</p>
+          </div>
+        </div>
+        <topic-reply></topic-reply>
+      </div>
+      <right-bar></right-bar>
+    </div>
   </div>
 </template>
 
 <script>
 import Header from '@/components/Header.vue'
+import RightBar from '@/components/RightBar.vue'
+import TopicReply from '@/components/TopicReply.vue'
 
 export default {
   name: 'TopicDetail',
   components: {
-    Header
+    Header,
+    RightBar,
+    TopicReply
+  },
+  data() {
+    return {
+      topic: {
+        src: require('@/assets/avatar.jpg')
+      }
+    }
   }
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import '@/scss/variable.scss';
+
+.container {
+  position: relative;
+  top: 80px;
+  word-break: break-all;
+}
+.left {
+  margin-right: 260px;
+  @media screen and (max-width: 768px) {
+    margin-right: 0;
+  }
+}
+.topic-top {
+  background: $color-white;
+  border-radius: 4px;
+  margin-bottom: 20px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1);
+}
+.topic-header {
+  position: relative;
+  min-height: 120px;
+  border-bottom: 1px solid #e2e2e2;
+}
+.header-left {
+  display: flex;
+  padding: 20px 100px 20px 10px;
+  text-align: left;
+  .title {
+    color: #333;
+    font-weight: bold;
+    line-height: 2rem;
+    font-size: 1.5rem;
+  }
+}
+.avatar {
+  width: 80px;
+  height: 80px;
+  border-radius: 4px;
+  position: absolute;
+  right: 10px;
+  top: 10px;
+}
+.topic-content {
+  padding: 20px 10px;
+  line-height: 1.5rem;
+  text-align: left;
+}
+</style>
